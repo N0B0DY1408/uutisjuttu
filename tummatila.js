c5f413f3809350d8d67af7a708106e7b4500be0e
N0B0DY1408 <tuikkamatias@gmail.com>
2025-11-17 12:10:10 +0200

    tummatila ja nappi parannuksia

---
+++ tummatila.js
@@ -0,0 +1,38 @@
+const button = document.getElementById("tummatilanappi");
+const html = document.documentElement;
+
+let theme;
+
+const setTheme = (value) => {
+    if (value === "dark") {
+        html.classList.add("tummatila");
+        button.textContent = "Vaalea tila";
+    } else {
+        html.classList.remove("tummatila");
+        button.textContent = "Tumma tila";
+    }
+
+    theme = value;
+    localStorage.setItem("theme", value);
+};
+
+const loadTheme = () => {
+    const saved = localStorage.getItem("theme");
+
+    if (saved) {
+        setTheme(saved);
+    } else {
+        setTheme("light");
+        localStorage.setItem("theme", "light");
+    }
+};
+
+button.addEventListener("click", () => {
+    if (theme === "dark") {
+        setTheme("light");
+    } else {
+        setTheme("dark");
+    }
+});
+
+loadTheme();
