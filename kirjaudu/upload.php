196bdc174bf378ef9968440ed9d1966f68abcd16
N0B0DY1408 <tuikkamatias@gmail.com>
2025-11-27 11:48:31 +0200

    EI VITTU SE TOIMII!!!!

--- kirjaudu/upload.php
+++ kirjaudu/upload.php
@@ -14,22 +14,25 @@ $fileSize =     $_FILES['file']['size'];
 $fileError =    $_FILES['file']['error'];
 $fileType =     $_FILES['file']['type'];
 
-$fileExt = explode('.', $filename);
+$fileExt = explode('.', $file);
 $fileActualExt = strtolower(end($fileExt));
 
 $allowed = array('jpg', 'png', 'jpeg');
 
 if (in_array($fileActualExt, $allowed)) {
     if ($fileError === 0) {
-        if ($fileSize < 5000000) {
+        if ($fileSize < 500000000) {
             $fileNameNew = $random.".".$fileActualExt;
-            $fileDestination = 'images/'.$fileNameNew;
+            $fileDestination = '../kuvat/'.$fileNameNew;
             move_uploaded_file($fileTmName, $fileDestination);
             echo "kuvan siirto onnistui";
             $sql = "INSERT INTO uutinen (otsikko, teksti, kuva, kategoria, toimittaja) VALUES ('$otsikko', '$teksti', '$fileNameNew', 3, 1)";
-            mysqli_query($conn, $sql);    
+            mysqli_query($conn, $sql);
+            header("location: ../uutisjuttu.php");    
+        } else {
+            echo "kuva on liian suuri";
         }
-        echo "kuva on liian suuri";
+        
     }
 }
 ?>
